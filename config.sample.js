3035ab658a48e8e408921329f67c6eab67de8f53
MrrVit665 <76064400+MrrVit665@users.noreply.github.com>
2021-08-26 07:49:45 +0700

    Create config.sample.js

---
+++ config.sample.js
@@ -0,0 +1,13 @@
+{
+    "token":"your discord bot secret token",
+    "prefix":"!",
+    "game":"made by JohnDavid",
+    "embed":{
+        "color":"#ff5b5b",
+        "footer":"Royal Development"
+    },
+    "administrators":[
+        "AdminID",
+        "AdminID"
+    ]
+}
